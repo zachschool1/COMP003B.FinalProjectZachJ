ee9a4e08264df302308d8ca5c7147bb34d16c327
zachschool1 <zacharyjordan@my.whccd.edu>
2026-05-06 21:51:33 -0700

    add all partials and fix minor issues. still need to fix all edit/delete etc to ensure all are correctly correlating. then need to do the api

---
+++ Views/Shared/_GamesPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.SP26.FinalProject.ZachJ.Views.Shared
+{
+    public class _GamesPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
