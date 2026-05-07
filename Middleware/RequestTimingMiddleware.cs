04606273348b5cfbd809f6f52471883c781866ce
zachschool1 <zacharyjordan@my.whccd.edu>
2026-05-07 08:27:33 -0700

    add custom middleware that shows method, path and execution time in miliseconds

---
+++ Middleware/RequestTimingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace COMP003B.SP26.FinalProject.ZachJ.Middleware
+{
+    public class RequestTimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestTimingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //method path and execution time
+            var clock = Stopwatch.StartNew();
+            
+
+            await _next(context);
+
+            clock.Stop();
+            
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}\n[Execution Time] {clock.Elapsed.Milliseconds} ms.");
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // Course: COMP-003B: ASP.NET Core
 // Instructor: Jonathan Cruz
 // Purpose: Final project synthesizing MVC, Web API, EF Core, and middleware
-
+using COMP003B.SP26.FinalProject.ZachJ.Middleware;
 using COMP003B.SP26.FinalProject.ZachJ.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -32,7 +32,8 @@ namespace COMP003B.SP26.FinalProject.ZachJ
 
             app.UseHttpsRedirection();
             app.UseRouting();
-
+            //custom middleware
+            app.UseMiddleware<RequestTimingMiddleware>();
             app.UseAuthorization();
 
             app.MapStaticAssets();
